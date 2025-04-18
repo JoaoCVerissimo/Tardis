5aedf8b32ceae538e4c68c17f3833d48223d9786
JVerissimo <j.verissimo@runtime-revolution.com>
2025-04-18 20:52:04 +0100

    feat: update next.config.ts to use remotePatterns for image domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['openweathermap.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+      },
+    ],
   },
 }
 
